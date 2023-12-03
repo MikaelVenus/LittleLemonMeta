43dad1022f29102ccb707a6658d8f61214018c23
Sun <nantawatc99@gmail.com>
2023-12-03 10:54:55 +0700

    addBookingView

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import MenuItemSerializer,BookingViewSerializer
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingViewSerializer
